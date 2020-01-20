7cd5309083895204b0d0ab755027c973230e5e98
TokToBeka <chenyl1680@gmail.com>
2020-01-20 19:01:09 +0800

    分类模块页面开发

--- youguoCloud/miniprogram/pages/classification/classification.js
+++ youguoCloud/miniprogram/pages/classification/classification.js
@@ -1,66 +1,108 @@
 // miniprogram/pages/classification/classification.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
+    categories: [],
+    categorySelected: {
+      name: '',
+      id: ''
+    },
+    currentGoods: [],
+    onLoadStatus: true,
+    scolltop: 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.categories()
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  async categories() {
+    wx.showLoading({
+      title: '加载中'
+    })
+    const res = {
+      "code": 0,
+      "data": [{
+        "id": 88042,
+        "key": "1",
+        "name": "活动",
+        "pid": 0,
+      }, {
+        "id": 88043,
+        "key": "2",
+        "name": "热销",
+        "pid": 0,
+      }, {
+        "id": 88044,
+        "key": "4",
+        "name": "时令水果",
+        "pid": 0,
+      }, {
+        "id": 88045,
+        "key": "5",
+        "name": "热带鲜果",
+        "pid": 0,
+      }, {
+        "id": 88046,
+        "key": "6",
+        "name": "进口水果",
+        "pid": 0,
+      }, {
+        "id": 88047,
+        "key": "7",
+        "name": "国产水果",
+        "pid": 0,
+      }, {
+        "id": 88048,
+        "key": "8",
+        "name": "优质水果",
+        "pid": 0,
+      }, {
+        "id": 88049,
+        "key": "9",
+        "name": "水果现切",
+        "pid": 0,
+      }, {
+        "id": 88050,
+        "key": "10",
+        "name": "时令礼篮",
+        "pid": 0,
+      }, {
+        "id": 88051,
+        "key": "11",
+        "name": "营养果干",
+        "pid": 0,
+      }],
+      "msg": "success"
+    };
+    wx.hideLoading()
+    let categories = [];
+    let categoryName = '';
+    let categoryId = '';
+    if (res.code == 0) {
+      for (let i = 0; i < res.data.length; i++) {
+        let item = res.data[i];
+        categories.push(item);
+        if (i == 0) {
+          categoryName = item.name;
+          categoryId = item.id;
+        }
+      }
+    }
+    this.setData({
+      categories: categories,
+      categorySelected: {
+        name: categoryName,
+        id: categoryId
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+  onCategoryClick: function (e) {
 
   }
+
 })
