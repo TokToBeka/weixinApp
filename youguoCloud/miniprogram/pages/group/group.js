9274659a3ff3348e8b0c6ff01945e198e0e7653d
TokToBeka <chenyl1680@gmail.com>
2020-05-15 10:00:33 +0800

    团购静态页面

--- youguoCloud/miniprogram/pages/group/group.js
+++ youguoCloud/miniprogram/pages/group/group.js
@@ -5,6 +5,47 @@ Page({
    * 页面的初始数据
    */
   data: {
+    pingtuanList: [{
+      'pic': 'cloud://youguo-7vd16.796f-youguo-7vd16-1301087841/swiper-image/banner-1.jpg',
+      'name': '台湾凤梨',
+      'characteristic': '宝岛果王，密味芳香',
+      'tags': '',
+      'pingtuanPrice': 59,
+      'originalPrice': 89,
+      'id': 10000
+    }, {
+      'pic': 'cloud://youguo-7vd16.796f-youguo-7vd16-1301087841/swiper-image/banner-9.jpg',
+      'name': '西班牙牛油果',
+      'characteristic': '百搭天王，妈妈之选',
+      'tags': '',
+      'pingtuanPrice': 129,
+      'originalPrice': 149,
+      'id': 10001
+    }, {
+      'pic': 'cloud://youguo-7vd16.796f-youguo-7vd16-1301087841/swiper-image/banner-7.jpg',
+      'name': '西班牙皇后红玫瑰苹果',
+      'characteristic': '浪漫之果，幸福味道',
+      'tags': '',
+      'pingtuanPrice': 59,
+      'originalPrice': 89,
+      'id': 10002
+    }, {
+      'pic': 'cloud://youguo-7vd16.796f-youguo-7vd16-1301087841/swiper-image/banner-2.jpg',
+      'name': '新奇士柠檬',
+      'characteristic': '品牌保证，酸甜可口',
+      'tags': '',
+      'pingtuanPrice': 29,
+      'originalPrice': 49,
+      'id': 10003
+    }, {
+      'pic': 'cloud://youguo-7vd16.796f-youguo-7vd16-1301087841/swiper-image/banner-8.jpg',
+      'name': '泰国芒果',
+      'characteristic': '热带果王，美容健康',
+      'tags': '',
+      'pingtuanPrice': 49,
+      'originalPrice': 69,
+      'id': 10004
+    }]
 
   },
 
