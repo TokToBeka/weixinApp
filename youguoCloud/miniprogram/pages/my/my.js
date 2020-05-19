d23c87befc1bd8dd0ae46aa5a77c442a064e7e17
TokToBeka <chenyl1680@gmail.com>
2020-05-19 15:26:14 +0800

    我的静态页面

--- youguoCloud/miniprogram/pages/my/my.js
+++ youguoCloud/miniprogram/pages/my/my.js
@@ -12,14 +12,36 @@ Page({
       'url': '../../assets/img/pay.png',
       'title': '待付款'
     }, {
-      'url': '../../assets/img/Receipt.png',
+      'url': '../../assets/img/pay.png',
       'title': '待收货'
     }, {
-      'url': '../../assets/img/evaluation.png',
+      'url': '../../assets/img/pay.png',
       'title': '待评价'
     }, {
-      'url': '../../assets/img/afterSale.png',
+      'url': '../../assets/img/order.png',
+      'title': '全部订单'
+    }],
+    serviceList: [{
+      'url': '../../assets/img/pay.png',
+      'title': '积分商城'
+    }, {
+      'url': '../../assets/img/pay.png',
+      'title': '领券中心'
+    }, {
+      'url': '../../assets/img/pay.png',
+      'title': '我的红包'
+    }, {
+      'url': '../../assets/img/pay.png',
+      'title': '我的评价'
+    }, {
+      'url': '../../assets/img/pay.png',
+      'title': '收货地址'
+    }, {
+      'url': '../../assets/img/pay.png',
       'title': '退换/售后'
+    }, {
+      'url': '../../assets/img/pay.png',
+      'title': '联系客服'
     }]
   },
 
